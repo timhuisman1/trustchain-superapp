97fcffc4b73b64c7773660b3de92d7ec8126495b
Lesley Franschman <l.franschman@student.tudelft.nl>
2023-04-12 15:05:05 +0200

    add single token transaction buttion and functionality + unit test

--- detoks/src/test/java/nl/tudelft/trustchain/debug/ExampleUnitTest.kt
+++ detoks/src/test/java/nl/tudelft/trustchain/debug/ExampleUnitTest.kt
@@ -1,7 +1,10 @@
 package nl.tudelft.trustchain.debug
 
+import nl.tudelft.trustchain.detoks.Token
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.Before
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,3 +17,16 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 }
+class TokenTest {
+
+    @Test
+    fun `test toString`() {
+        val uniqueId = "12345"
+        val publicKey = byteArrayOf(1)
+        val token = Token(uniqueId, publicKey)
+
+        assertEquals("$uniqueId,${publicKey}", token.toString())
+    }
+}
+
+
